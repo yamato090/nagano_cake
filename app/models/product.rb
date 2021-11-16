cb43a342d695a3fa6a1e5e59ae6a9181adb45b12
ryu57sp <ryuki.baske5@gmail.com>
2021-11-16 13:19:38 +0000

    [Add]商品関連機能

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  belongs_to :genre
+  attachment :image
+
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :detail, presence: true
+  validates :tax_excluded_price, presence: true
+  validates :is_active, inclusion: [true, false]
+
+  def add_tax_included_price
+    (self.tax_excluded_price * 1.10).round
+  end
 end
