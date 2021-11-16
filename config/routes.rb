0de4379c236260cfd2ad8749e86ec38fe481ad72
ryu57sp <ryuki.baske5@gmail.com>
2021-11-16 06:15:13 +0000

    MVC_Gem_ルーティング実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  
+
   # 会員側のルーティング設定
-  root to: "/"
-  root to: "/about"
-  
+  root to: "public/homes#top"
+
   devise_for :customer, skip:[:passwords], controllers:{
     registrations: "customer/registrations",
     sessions: "customer/sessions"
   }
-  
+
   # 管理者側のルーティング設定
   namespace :admin do
     devise_for :admin, skip:[:registrations, :passwords], controllers:{
@@ -16,18 +15,18 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
-    root to: 'homes/top'
+    root to: 'homes#top'
     resources :products, except:[:destroy]
     resources :genres, only:[:index, :edit, :create, :update]
     resources :customers, only:[:index, :show, :edit, :update]
     resources :orders, only:[:index, :show, :update]
     resources :order_details, only:[:update]
   end
-  
-  
-  
-  
-  
+
+
+
+
+
   end
 
 end
