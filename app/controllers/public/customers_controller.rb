118c5a9214be4ead7d95b48dc27249203e8ad53c
marishikaw <90078966+marishikaw@users.noreply.github.com>
2021-11-17 12:42:36 +0000

    [Updating]customer_n_delivery追加中

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,8 +4,38 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
+  end
+
+  def update
+    customer = current_customer
+    if customer.update(customer_params)
+      redirect_to customers_path
+    else
+      redirect_to request.referer
+    end
   end
 
   def quit
+    customer = current_customer
+    if customer.update(is_deleted: true)
+      reset_session
+      redirect_to root_path
+    end    
   end
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, 
+                                     :first_name, 
+                                     :last_name_kana, 
+                                     :first_name_kana, 
+                                     :post_code, 
+                                     :address, 
+                                     :phone_number, 
+                                     :email, 
+                                     :is_deleted)
+  end  
+  
 end
