c83b20db15ccc203514dc627fa634f29487b2a87
sin0713 <narisin1124@gmail.com>
2021-11-18 02:53:45 +0000

    [Add]注文履歴一覧と、seedsファイルの追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,15 @@
 class Admin::OrdersController < ApplicationController
+  layout 'admin'
+
   def index
+   if params[:customer_id]
+     @orders = Order.where(customer_id: params[:customer_id]).page(params[:page]).per(10)
+   else
+     @orders = Order.all.order(created_at: :desc).page(params[:page]).per(10)
+   end
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 end
